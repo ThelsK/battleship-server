5e82c2f09dbef19506a7029853cde80f2477e9be
ThelsK <github@thels.nl>
2019-09-16 16:17:23 +0200

    Create example data in order to prevent error

--- src/example.js
+++ src/example.js
@@ -8,6 +8,51 @@ const Square = require("./square/model");
 const exampleData = () => {
   Room.findOrCreate({
     where: { roomname: "Fruit" }
+  }).then(() => {
+    AvailableShip.findOrCreate({
+      where: { length: 4 },
+      defaults: {
+        roomId: 1
+      }
+    });
+    Notification.findOrCreate({
+      where: { content: "Hello World!" },
+      defaults: {
+        roomId: 1
+      }
+    });
+    User.findOrCreate({
+      where: { username: "Apple" },
+      defaults: {
+        password: "XXX",
+        roomId: 1
+      }
+    }).then(() => {
+      Ship.findOrCreate({
+        where: { top_pos: 2 },
+        defaults: {
+          userId: 1,
+          left_pos: 4,
+          direction: "vert",
+          length: 4
+        }
+      });
+      Square.findOrCreate({
+        where: { hori_pos: 3 },
+        defaults: {
+          userId: 1,
+          status: "hit",
+          vert_pos: 5
+        }
+      });
+    });
+    User.findOrCreate({
+      where: { username: "Banana" },
+      defaults: {
+        password: "XXX",
+        roomId: 1
+      }
+    });
   });
   Room.findOrCreate({
     where: { roomname: "Veggies" }
@@ -17,49 +62,6 @@ const exampleData = () => {
   });
   Room.findOrCreate({
     where: { roomname: "Fish" }
-  });
-  AvailableShip.findOrCreate({
-    where: { length: 4 },
-    defaults: {
-      roomId: 1
-    }
-  });
-  Notification.findOrCreate({
-    where: { content: "Hello World!" },
-    defaults: {
-      roomId: 1
-    }
-  });
-  User.findOrCreate({
-    where: { username: "Apple" },
-    defaults: {
-      password: "XXX",
-      roomId: 1
-    }
-  });
-  User.findOrCreate({
-    where: { username: "Banana" },
-    defaults: {
-      password: "XXX",
-      roomId: 1
-    }
-  });
-  Ship.findOrCreate({
-    where: { top_pos: 2 },
-    defaults: {
-      userId: 1,
-      left_pos: 4,
-      direction: "vert",
-      length: 4
-    }
-  });
-  Square.findOrCreate({
-    where: { hori_pos: 3 },
-    defaults: {
-      userId: 1,
-      status: "hit",
-      vert_pos: 5
-    }
   });
 };
 
