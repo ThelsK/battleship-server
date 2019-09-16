08cbaedb8f6de922b5c12a63f2fa59e76f80abbd
ThelsK <github@thels.nl>
2019-09-16 12:10:32 +0200

    Setup server framework

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const app = require("express")()
+const cors = require("cors")()
+const bodyParser = require("body-parser").json()
+
+app.use(
+  cors,
+  bodyParser
+)
+
+const port = process.env.PORT || 4000
+app.get("/", (req, res) => res.send("Hello World!"))
+app.listen(port, () => console.log(`Listening on :${port}`))
